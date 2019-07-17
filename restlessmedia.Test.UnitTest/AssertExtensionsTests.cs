fa8f98f721b13145bcde5dd7eddfcdca14b99cad
Philip Cooper <phillycooper@hotmail.com>
2019-07-17 16:43:06 +0100

    Fixing issues & adding tests.

--- restlessmedia.Test.UnitTest/AssertExtensionsTests.cs
+++ restlessmedia.Test.UnitTest/AssertExtensionsTests.cs
@@ -1,4 +1,5 @@
-﻿using Xunit;
+﻿using System;
+using Xunit;
 
 namespace restlessmedia.Test.UnitTest
 {
@@ -7,7 +8,86 @@ namespace restlessmedia.Test.UnitTest
     [Fact]
     public void MustBeLike()
     {
-      "foo".MustBeLike("bar");
+      "foo".MustBeLike("foo");
+    }
+
+    [Fact]
+    public void MustNotBeLike()
+    {
+      "foo".MustNotBeLike("bar");
+    }
+
+    [Fact]
+    public void MustBeNull()
+    {
+      ((string)null).MustBeNull();
+    }
+
+    [Fact]
+    public void MustNotBeNull()
+    {
+      "foo".MustNotBeNull();
+      DateTime.Now.MustNotBeNull();
+    }
+
+    [Fact]
+    public void Fail()
+    {
+      Assert.Throws<Exception>(() => AssertExtensions.Fail(""));
+    }
+
+    [Fact]
+    public void MustContain()
+    {
+      new[] { "foo", "bar" }.MustContain("foo", "bar");
+    }
+
+    [Fact]
+    public void MustNotContain()
+    {
+      new[] { "foo", "bar" }.MustNotContain("one", "two");
+    }
+
+    [Fact]
+    public void MustBeTrue()
+    {
+      true.MustBeTrue();
+    }
+
+    [Fact]
+    public void MustBeFalse()
+    {
+      false.MustBeFalse();
+    }
+
+    [Fact]
+    public void MustBeA()
+    {
+      DateTime.Now.MustBeA<DateTime>();
+    }
+
+    [Fact]
+    public void MustBeA_type()
+    {
+      typeof(DateTime).MustBeA<DateTime>();
+    }
+
+    [Fact]
+    public void MustNotThrow()
+    {
+      new Action(() => { }).MustNotThrow();
+    }
+
+    [Fact]
+    public void MustThrow()
+    {
+      new Action(() => throw new Exception()).MustThrow();
+    }
+
+    [Fact]
+    public void MustMatch()
+    {
+      typeof(DateTime).MustMatch<DateTime>();
     }
   }
 }
--- src/AssertExtensions.cs
+++ src/AssertExtensions.cs
@@ -3,70 +3,134 @@ using System.Collections.Generic;
 using System.Linq;
 using Xunit;
 
-namespace restlessmedia.Module.Product.UnitTest
+namespace restlessmedia.Test
 {
   public static class AssertExtensions
   {
+    /// <summary>
+    /// Throws exception with given message.
+    /// </summary>
+    /// <param name="message"></param>
     public static void Fail(string message)
     {
       throw new Exception($"Assert failed with {message}");
     }
 
+    /// <summary>
+    /// Values are equal.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="actual"></param>
+    /// <param name="expected"></param>
     public static void MustBe<T>(this T actual, T expected)
     {
       Assert.Equal(expected, actual);
     }
 
+    /// <summary>
+    /// Value isn't null.
+    /// </summary>
+    /// <param name="actual"></param>
     public static void MustNotBeNull(this object actual)
     {
       Assert.NotNull(actual);
     }
 
-    public static void MustBeLike(this string actual, string expected)
+    /// <summary>
+    /// The string does equal given the provided <see cref="StringComparison"/>.
+    /// </summary>
+    /// <param name="actual"></param>
+    /// <param name="expected"></param>
+    /// <param name="stringComparison"></param>
+    public static void MustBeLike(this string actual, string expected, StringComparison stringComparison = StringComparison.OrdinalIgnoreCase)
     {
-      Assert.False(string.Equals(actual, expected, StringComparison.OrdinalIgnoreCase));
+      MustBeTrue(string.Equals(actual, expected, stringComparison));
     }
 
-    public static void MustNotBeLike(this string actual, string expected)
+    /// <summary>
+    /// The string does not equal given the provided <see cref="StringComparison"/>.
+    /// </summary>
+    /// <param name="actual"></param>
+    /// <param name="expected"></param>
+    /// <param name="stringComparison"></param>
+    public static void MustNotBeLike(this string actual, string expected, StringComparison stringComparison = StringComparison.OrdinalIgnoreCase)
     {
-      Assert.False(string.Equals(actual, expected, StringComparison.OrdinalIgnoreCase));
+      MustBeFalse(string.Equals(actual, expected, stringComparison));
     }
 
+    /// <summary>
+    /// Value must be null.
+    /// </summary>
+    /// <param name="actual"></param>
     public static void MustBeNull(this object actual)
     {
-      Assert.NotNull(actual);
+      Assert.Null(actual);
     }
 
+    /// <summary>
+    /// The list does contain the expected values.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="actual"></param>
+    /// <param name="expected"></param>
     public static void MustContain<T>(this IEnumerable<T> actual, params T[] expected)
     {
       Assert.False(actual.Except(expected).Any());
     }
 
+    /// <summary>
+    /// The list does not contain the expected values.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="actual"></param>
+    /// <param name="expected"></param>
     public static void MustNotContain<T>(this IEnumerable<T> actual, params T[] expected)
     {
       Assert.False(actual.Intersect(expected).Any());
     }
 
+    /// <summary>
+    /// The value is true.
+    /// </summary>
+    /// <param name="actual"></param>
     public static void MustBeTrue(this bool actual)
     {
       Assert.True(actual);
     }
 
+    /// <summary>
+    /// The value is false.
+    /// </summary>
+    /// <param name="actual"></param>
     public static void MustBeFalse(this bool actual)
     {
       Assert.False(actual);
     }
 
+    /// <summary>
+    /// The type is assignable from <see cref="T"/>.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="type"></param>
     public static void MustBeA<T>(this Type type)
     {
       Assert.True(typeof(T).IsAssignableFrom(type));
     }
 
+    /// <summary>
+    /// Obj is assignable from <see cref="T"/>.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="obj"></param>
     public static void MustBeA<T>(this object obj)
     {
       MustBeA<T>(obj.GetType());
     }
 
+    /// <summary>
+    /// The action does not throw.
+    /// </summary>
+    /// <param name="action"></param>
     public static void MustNotThrow(this Action action)
     {
       Exception exception = null;
@@ -83,6 +147,11 @@ namespace restlessmedia.Module.Product.UnitTest
       exception.MustBeNull();
     }
 
+    /// <summary>
+    /// The action throws <see cref="T"/>.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="action"></param>
     public static void MustThrow<T>(this Action action)
       where T : Exception
     {
@@ -100,14 +169,23 @@ namespace restlessmedia.Module.Product.UnitTest
       exception.MustNotBeNull();
     }
 
+    /// <summary>
+    /// The action throws.
+    /// </summary>
+    /// <param name="action"></param>
     public static void MustThrow(this Action action)
     {
       MustThrow<Exception>(action);
     }
 
+    /// <summary>
+    /// The types equals <see cref="T"/>.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="type"></param>
     public static void MustMatch<T>(this Type type)
     {
-      MustBeTrue(typeof(T) == type);
+      MustBe(typeof(T), type);
     }
   }
 }
